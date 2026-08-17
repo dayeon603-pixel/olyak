ec4a275501478e3090cd34ebea6c70431c705830
dayeon603-pixel <dayeon603@gmail.com>
2026-08-17 18:23:09 +0900

    print: 추첨 경품을 프로 10개월로 조정
    가액 49,000원으로 5만원 이하를 유지해 기타소득 원천징수 신고 의무를 없앴다.
    3년(176,400원)이면 22퍼센트 원천징수 신고가 생긴다. 전원 지급분 3개월
    (14,700원)도 같은 이유로 해당 없다.
    
    - 응답자 전원: 출시 후 프로 3개월 무료
    - 추첨 1명: 프로 10개월 이용권

--- print/build_leaflet.py
+++ print/build_leaflet.py
@@ -331,7 +331,7 @@ HTML = f"""<meta charset="utf-8">
       <h3>사용 후기를 들려주세요</h3>
       <p>더 쓰기 편한 올약을 만드는 데<br>큰 도움이 됩니다. 3분이면 됩니다.</p>
       <div class="event">응답해 주신 모든 분께 출시 후 프로 3개월 무료
-        <small>추첨으로 1분께 프로 3년 이용권을 드립니다</small></div>
+        <small>추첨으로 1분께 프로 10개월 이용권을 드립니다</small></div>
     </div>
 
     <div class="notice">
